10d3475bc347a3c26b039db0771b6e0c22398aa7
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-12 23:05:43 +0530

    Configure new color variables and font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "var(--color-primary)",
+        "primary-hover": "var(--color-primary-hover)",
+        accent: "var(--color-accent)",
+        "accent-hover": "var(--color-accent-hover)",
+        "text-main": "var(--color-text-main)",
+        "text-sm": "var(--color-text-sm)",
+        sidebar: "var(--color-sidebar)",
+        card: "var(--color-card)",
+        "card-2": "var(--color-card-2)",
+        bg: "var(--color-bg)",
+
+        "input-border": "var(--color-input-border)",
+        "input-border-active": "var(--color-input-border-active)",
+        "input-fill": "var(--color-input-fill)",
+        "green-custome": "var(--color-green)",
+        "orange-custom": "var( --color-orange)",
+      },
+      fontFamily: {
+        primary: "var(--font-league-spartan)",
       },
     },
   },
