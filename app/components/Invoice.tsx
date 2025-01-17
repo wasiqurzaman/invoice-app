145b4c7658f63fa48a790756498d5ada96609d11
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-17 18:44:59 +0530

    Add Invoice component to display invoice details

---
+++ app/components/Invoice.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { InvoiceType } from "../invoices/page";
+import { format } from "date-fns";
+import InvoiceStatus from "./IncoiceStatus";
+import Link from "next/link";
+
+interface Props {
+  invoice: InvoiceType;
+}
+
+export default function Invoice({ invoice }: Props) {
+  return (
+    <Link
+      href={`/invoices/${invoice.id}`}
+      className="w-full h-[72px] pl-[32px] pr-[24px] py-[16px] bg-card flex items-center justify-between rounded-[8px] gap-10 shadow-sm"
+    >
+      <div>
+        <span className="heading-sm text-textCol-3">#</span>
+        <span className="heading-sm">{invoice.id}</span>
+      </div>
+      <span className="text-textCol-2">
+        Due {format(new Date(invoice.paymentDue), "dd MMM yyyy")}
+      </span>
+      <span className="text-textCol-2">{invoice.clientName}</span>
+      <p className="heading-sm-v">$ {invoice.total.toFixed(2)}</p>
+      <div className="flex items-center gap-5">
+        <InvoiceStatus status={invoice.status} />
+        <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M1 1l4 4-4 4"
+            stroke="#7C5DFA"
+            strokeWidth="2"
+            fill="none"
+            fillRule="evenodd"
+          />
+        </svg>
+      </div>
+    </Link>
+  );
+}
