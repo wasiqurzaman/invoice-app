24c5c3bbbe46d0121e056eed86332ae7e6e5902c
Wasiqur Zaman <wasiqurzaman@gmail.com>
2025-01-18 23:58:40 +0530

    Implement API route for retrieving a specific invoice by ID

---
+++ app/api/invoices/[id]/route.tsx
@@ -0,0 +1,25 @@
+import prisma from "@/prisma/prismaClient";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  const invoice = await prisma.invoice.findUnique({
+    where: {
+      invoiceId: id,
+    },
+    include: {
+      clientAddress: true,
+      senderAddress: true,
+      items: true,
+    },
+  });
+
+  if (!invoice)
+    return NextResponse.json({ error: "invoice not found" }, { status: 400 });
+
+  return NextResponse.json(invoice, { status: 200 });
+}
